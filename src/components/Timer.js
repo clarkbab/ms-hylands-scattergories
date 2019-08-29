a72e89f6d9b532019bf6f2eef6c2e373edbc436c
Brett Clark <breclark@zendesk.com>
2019-08-29 12:02:16 +1000

    Add timer reset

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -10,44 +10,98 @@ const baseStyles = {
   width: '50px',
 }
 
-const period = 5
+const states = {
+  PAUSED: 0,
+  RUNNING: 1,
+  RESET: 2
+}
+
+const startTime = 5
+const resetTime = 1
 var interval
 
 const Timer = () => {
-  const [paused, setPaused] = useState(true)
-  const [time, setTime] = useState(period)
-  console.log(`render time: ${time}`)
+  const [state, setState] = useState({
+    timerState: states.PAUSED,
+    time: startTime,
+    resetTime: resetTime
+  })
+  console.log(`rendering. time: ${state.time}. resetTime: ${state.resetTime}`)
 
+  // Starts the countdown timer.
   const startTimer = () => {
     console.log('starting timer')
-    setPaused(p => false)
-    interval = setInterval(() => {
-      setTime(t => t - 1) 
-    }, 1000)
+    setState(s => ({ ...s, timerState: states.RUNNING }))
+    interval = setInterval(tickHandler, 1000)
   }
 
+  // Pauses the countdown timer.
   const pauseTimer = () => {
     console.log('pausing timer')
-    setPaused(p => true)
+    setState(s => ({ ...s, timerState: states.PAUSED }))
     clearInterval(interval)
+  }
+
+  // Performs the reset stage of the countdown timer.
+  const resetTimer = () => {
+    console.log('resetting timer')
+    setState(s => ({ ...s, timerState: states.RESET }))
   }
 
   // Handles each timer click.
   const clickHandler = () => {
-    if (paused) {
-      startTimer()
-    } else {
-      pauseTimer()
+    switch (state.timerState) {
+      // If timer is in RESET, we can't click on it.
+      case states.PAUSED:
+        startTimer()
+        break
+      case states.RUNNING:
+        pauseTimer()
+        break
+      default: break
     }
   }
 
+  // Handles each timer tick. 
+  const tickHandler = () => {
+    console.log('ticking')
+    setState(s => {
+      switch (s.timerState) {
+        case states.RUNNING:
+          // If timer is running, count down or reset. 
+          if (s.time > 0) {
+            return { ...s, time: s.time - 1 }
+          } else {
+            resetTimer()
+            return { ...s, time: 0, resetTime: s.resetTime - 1 }
+          }
+        case states.RESET: {
+          // If timer is in reset mode, count down to reset or reset.
+          if (s.resetTime > 0) {
+            return { ...s, resetTime: s.resetTime - 1 }
+          } else {
+            pauseTimer()
+            return { ...s, time: startTime, resetTime: resetTime }
+          }
+        }
+        default: return s
+      }
+    })
+  }
+
   // Get background colour.
-  var backColor = paused ? 'pink' : 'lightgreen'
+  let backColor
+  switch (state.timerState) {
+    case states.PAUSED: backColor = 'pink'; break
+    case states.RUNNING: backColor = 'lightgreen'; break
+    case states.RESET: backColor = 'orange'; break
+    default: break
+  }
   var styles = Object.assign({ backgroundColor: backColor }, baseStyles)
 
   return (
     <div>
-      <div style={styles} onClick={clickHandler}>{time}s</div>
+      <div style={styles} onClick={clickHandler}>{state.time}s</div>
     </div>
   )
 }
